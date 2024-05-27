616c69c621d84c8bb8a5362ac5500e718a0e269b
Neelesh Joshi <neeleshjoshi001@gmail.com>
2024-05-27 22:10:55 +0530

    added cron job to app

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import appRouter from './routes/routes.js';
+import { startCronJobs } from './utils/cronjob.js';
 const app = express();
 const PORT_NUM = 8000;
-export const STRAPI_URL = "http://strapi.koders.in/api/expenses"
+export const STRAPI_URL = "http://strapi.koders.in/api/expenses";
 
-app.use(express.json())
-app.use('/', appRouter)
+app.use(express.json());
+app.use('/', appRouter);
+
+startCronJobs();
 
 app.listen(PORT_NUM,()=>{
     console.log('... starting the app');
